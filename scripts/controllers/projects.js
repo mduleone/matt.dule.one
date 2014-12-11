3435aff59f958449be494b680fef5e8711276e12
mduleone <matt@duleone.com>
2014-12-11 01:04:08 -0500

    Remove the duplicate craps card

--- scripts/controllers/projects.js
+++ scripts/controllers/projects.js
@@ -6,9 +6,10 @@ angular.module("app")
             title: "Casino Craps",
             href: "http://duleone.com/craps",
             desc: "A Front-End implementation of the popular Casino table game, Craps! Complete with a tutorial, this is an excellent and fun way to learn to play Craps without the fear of losing your shirt! You start with $10,000, and table stakes allow for you to be as frugal or as extravigant as you wish, allowing bets of all sizes, from $10 to $10,000."
-        }, {
-            title: "Casino Craps",
-            href: "http://duleone.com/craps",
-            desc: "A Front-End implementation of the popular Casino table game, Craps! Complete with a tutorial, this is an excellent and fun way to learn to play Craps without the fear of losing your shirt! You start with $10,000, and table stakes allow for you to be as frugal or as extravigant as you wish, allowing bets of all sizes, from $10 to $10,000."
         },]
+        // {
+        //     title: "Poker",
+        //     href: "http://duleone.com/playholdem",
+        //     desc: "Pker!!!"
+        // },]
     });
