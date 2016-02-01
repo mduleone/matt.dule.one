434646833fc02ac4da5e5307918b0f973e0c4405
Matt DuLeone <matt@duleone.com>
2016-02-01 01:08:06 -0500

    projects

--- scripts/app.js
+++ scripts/app.js
@@ -22,4 +22,9 @@ angular.module('app', ['ngRoute'])
             .otherwise({
                 redirectTo: '/'
             });
+    }])
+    .filter('rawHtml', ['$sce', function($sce){
+      return function(val) {
+        return $sce.trustAsHtml(val);
+      };
     }]);
