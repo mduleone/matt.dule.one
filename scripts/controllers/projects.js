7d8d345d90a8bd5f337e13ce5561909c945f900d
Matt DuLeone <matt@duleone.com>
2016-02-01 00:56:48 -0500

    projects

--- scripts/controllers/projects.js
+++ scripts/controllers/projects.js
@@ -13,10 +13,20 @@ angular.module("app")
             repo: "https://github.com/mduleone/MakeUtterance",
             desc: "A very basic tool to make creating utterance strings for the Amazon Echo easier.",
         }, {
-            title: "Echo Pokedex",
+            title: "Pokedex - Alexa Skill",
             href: "",
             repo: "https://github.com/mduleone/EchoPokedex",
             desc: "A Pokedex skill for the Amazon Echo. Ask Alexa to tell you about any Pokemon in the Pokemon universe.",
+        }, {
+            title: "Node Poker",
+            href: "",
+            repo: "https://github.com/mduleone/nodePoker",
+            desc: "A Node.js implementation of a poker hand analyzer. Inspired by solving <a href='https://projecteuler.net/problem=54'>Project Euler #54</a> in <a href='https://gist.github.com/mduleone/133c118b8a6c6bb9b624'>COBOL</a> and wanting a more practical api.",
+        }, {
+            title: "Five Card Draw - Alexa Skill",
+            href: "",
+            repo: "https://github.com/mduleone/alexaFivecard",
+            desc: "A Five Card Draw Poker skill for the Amazon Echo. Ask Alexa to deal you a hand, and then try your best to beat her!<br /><br />Built on top of <a href='#'>Node Poker</a>",
         },]
         // {
         //     title: "Poker",
