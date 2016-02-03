c4db9c0feb59ddc2f3fba44b7d74c4abe5db16dd
Matt DuLeone <matt@duleone.com>
2016-02-03 02:26:16 -0500

    Project descriptions

--- scripts/controllers/projects.js
+++ scripts/controllers/projects.js
@@ -2,7 +2,8 @@
 
 angular.module("app")
     .controller("ProjectsController", function($scope) {
-        $scope.projects = [{
+        $scope.projects = [
+        {
             title: "Casino Craps",
             href: "http://duleone.com/craps",
             repo: "https://github.com/mduleone/craps",
@@ -11,22 +12,22 @@ angular.module("app")
             title: "Five Card Draw - Alexa Skill",
             href: "",
             repo: "https://github.com/mduleone/alexaFivecard",
-            desc: "A Five Card Draw Poker skill for the Amazon Echo. Ask Alexa to deal you a hand, and then try your best to beat her!<br /><br />Built on Node Poker.",
-        }, {
-            title: "Make Utterance",
-            href: "https://www.npmjs.com/package/make-utterance",
-            repo: "https://github.com/mduleone/MakeUtterance",
-            desc: "A very basic tool to make creating utterance strings for the Amazon Echo easier.",
+            desc: "A Five Card Draw Poker skill for the Amazon Echo. Ask Alexa to deal you a hand, and then try your best to beat her!<br /><br />Powered by <a href='http://dule.one/holdemAnalyzer' target='_blank'>Node Poker</a>.",
         }, {
             title: "Node Poker",
             href: "http://dule.one/holdemAnalyzer",
             repo: "https://github.com/mduleone/nodePoker",
             desc: "A Node.js implementation of a poker hand analyzer. <a href='/holdemAnalyzer' target='_blank'>Texas Hold'em</a> implemented in the GUI, <a href='/holdem?board=KsQsTsKdAd&hand1=AsJs&hand2=AhKh&hand5=TdTh' target='_blank'>Texas Hold'em</a> and <a href='poker?hand1=AsJsKsQsTs&hand2=AhKhAcKdAd' target='_blank'>5 card</a> poker implemented in an API. More games implemented in the project, but none with exposed endpoints. Inspired by solving <a href='https://projecteuler.net/problem=54' target='_blank'>Project Euler #54</a> in <a href='https://gist.github.com/mduleone/133c118b8a6c6bb9b624' target='_blank'>COBOL</a> and wanting a more practical API.",
         }, {
+            title: "Make Utterance",
+            href: "https://www.npmjs.com/package/make-utterance",
+            repo: "https://github.com/mduleone/MakeUtterance",
+            desc: "A very basic tool to make creating utterance strings for the Amazon Echo easier.",
+        }, {
             title: "Pokedex - Alexa Skill",
             href: "",
             repo: "https://github.com/mduleone/EchoPokedex",
-            desc: "A Pokedex skill for the Amazon Echo. Ask Alexa to tell you about any Pokemon in the Pokemon universe.",
+            desc: "A Pokedex skill for the Amazon Echo. Ask Alexa to tell you about any Pokemon in the Pokemon universe.<br /><br />Powered by the <a href='http://pokeapi.co/' target='_blank'>Pok&eacute;mon RESTful API</a>.",
         },]
         // {
         //     title: "Poker",
